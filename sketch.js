17fe06028ca63bb673429cb367d6cb3824ebaed2
Felicity520666 <yyan520666@gmail.com>
2026-08-18 14:56:47 -0400

    debug

--- sketch.js
+++ sketch.js
@@ -1,32 +1,44 @@
-let img;
-let size;
-let asciiChar = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'.";
-
-let video; let vidw = 64; let vidh = 48; let scl = 10;
+let video;
+let vidw = 80;
+let vidh = 60;
+let scl = 10;
 let w, h;
+
+let asciiChar = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
 
 function setup() {
     createCanvas(vidw * scl, vidh * scl);
     video = createCapture(VIDEO);
     video.size(vidw, vidh);
-    w = width / video.width;
-    h = height / video.height;
+    video.hide();
+    w = width / vidw;
+    h = height / vidh;
 }
 
 function draw() {
-    background(220);
+    background(0);
+    video.loadPixels();
 
-    for (let i=0; i<img.width; i++) {
-        for (let j=0; j<img.height; j++) {
-            let pixelVal = img.get(i, j);
+    for (let i = 0; i < video.width; i++) {
+        for (let j = 0; j < video.height; j++) {
+            let pixelVal = video.get(i, j);
+            let r = red(pixelVal);
+            let g = green(pixelVal);
+            let b = blue(pixelVal);
             let bright = (r + g + b) / 3;
-            let tIndex = floor(map(bright, 0, 255, 0, assciChar.length));
 
-            let x = i*w + w/2;
-            let y = j*h + h/2;
+            let tIndex = floor(map(bright, 0, 255, asciiChar.length - 1, 0));
+            tIndex = constrain(tIndex, 0, asciiChar.length - 1);
+
+            let x = i * w + w / 2;
+            let y = j * h + h / 2;
             let t = asciiChar.charAt(tIndex);
-            textSize(w)
-            textAlign(CENTER. CENTER);
+
+            fill(r, g, b);
+            noStroke();
+            textSize(w);
+            textAlign(CENTER, CENTER);
             text(t, x, y);
+        }
     }
 }
